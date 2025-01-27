6bd7cf019c341c93568955e93dbdab97c99b5e73
Josef1923 <schererjos01@gmail.com>
2025-01-27 13:03:58 +0100

    Ajout sidebar et placement différents elements (header/sidebar/dashboard)

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,22 @@
 import { BrowserRouter as Router } from "react-router-dom"
 import Header from "./components/header"
+import Sidebar from "./components/sidebar"
+import './App.scss'
 
 function App() {
   return (
     <Router>
-      <Header />
+      <div className="app">
+        <Header />
+        <div className="main">
+          <Sidebar />
+          <div className="dashboard">
+            <h1>SportSee</h1>
+          </div>
+        </div>
+      </div>
     </Router>
-  )
+  );
 }
 
 export default App
--- Frontend/src/components/sidebar/index.jsx
+++ Frontend/src/components/sidebar/index.jsx
@@ -1,22 +1,23 @@
-import logo from "../../assets/logo/logo.svg";
+import zen from "../../assets/icons/sidebar/zen.svg";
+import swim from "../../assets/icons/sidebar/swim.svg";
+import bike from "../../assets/icons/sidebar/bike.svg";
+import dumbbell from "../../assets/icons/sidebar/dumbbell.svg";
 import "./styles.scss";
 
 
-function Sidebar () {
+function Sidebar() {
     return (
-        <header className="header">
-            <div className="logo">
-                <img src={logo} alt="Logo SportSee" />
+        <div className="sidebar">
+            <ul>
+                <li><img src={zen} alt="icon zen" /></li>
+                <li><img src={swim} alt="icon nage" /></li>
+                <li><img src={bike} alt="icon velo" /></li>
+                <li><img src={dumbbell} alt="icon haltere" /></li>
+            </ul>
+            <div className="copyright">
+                <p>Copyright, SportSee 2020</p>
             </div>
-            <nav>
-                <ul>
-                    <li>Accueil</li>
-                    <li>Profil</li>
-                    <li>Réglage</li>
-                    <li>Communauté</li>
-                </ul>
-            </nav>
-        </header>
+        </div>
     )
 }
 
