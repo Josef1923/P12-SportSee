56d8e5e73f30c28c9dc6ae4d92f44f33453f03af
Josef1923 <schererjos01@gmail.com>
2025-02-05 16:15:39 +0100

    Envoie component Header et Sidebar dans Dashboard au lieu de mettre dans App

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,21 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import Header from "./components/header"
-import Sidebar from "./components/sidebar"
 import Dashboard from "./components/dashboard/dashboard"
 import './App.scss' 
 
 function App() {
   return (
     <Router>
-      <div className="app">
-        <Header />
-        <div className="main">
-          <Sidebar />
-          <Routes>
-            <Route path="/user/:id" element={<Dashboard/>}/>
-          </Routes>
-        </div>
-      </div>
+      <Routes>
+        <Route path="/user/:id" element={<Dashboard />} />
+      </Routes>
     </Router>
   );
 }
--- Frontend/src/components/dashboard/dashboard/index.jsx
+++ Frontend/src/components/dashboard/dashboard/index.jsx
@@ -1,21 +1,29 @@
 import { useParams } from "react-router-dom";
-import { useUser } from "../../../data/fetchUserDatas"; 
+import { useUser } from "../../../data/fetchUserDatas";
 import Welcome from "../welcome";
+import Header from "../../header";
+import Sidebar from "../../sidebar";
 
 function Dashboard() {
 
     const { id } = useParams();
-    const  userDatas = useUser(parseInt(id));
+    const userDatas = useUser(parseInt(id));
 
-    
+
     if (!userDatas) {
         return <div>Utilisateur introuvable</div>;
     }
 
 
     return (
-        <div className="dashboard">
-            <Welcome firstName={userDatas.userInfos.firstName} />         
+        <div className="app">
+            <Header/>
+            <div className="main">
+                <Sidebar/>
+                <div className="dashboard">
+                    <Welcome firstName={userDatas.userInfos.firstName} />
+                </div>
+            </div>
         </div>
     );
 }
