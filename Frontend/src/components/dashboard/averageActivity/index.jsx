9dd6efa33056fede75e19535f0c538a954e5b8d2
Josef1923 <schererjos01@gmail.com>
2025-02-12 22:11:36 +0100

    Ajout ombrage gauche graphique averageActivity

--- Frontend/src/components/dashboard/averageActivity/index.jsx
+++ Frontend/src/components/dashboard/averageActivity/index.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import { ResponsiveContainer, LineChart, Line, XAxis, Tooltip, YAxis } from "recharts";
+import { ResponsiveContainer, LineChart, Line, XAxis, Tooltip, YAxis, Rectangle } from "recharts";
 import "./styles.scss";
 
 
@@ -15,6 +15,30 @@ const CustomTooltip = ({ active, payload }) => {
     return null;
 };
 
+//Composant pour personnalisé Tooltip pour avoir le coté rouge dponcé à droite
+const CustomCursor = ({ points, width }) => {
+    const { x } = points[0];
+
+    return (
+        <Rectangle 
+        className="customCursor"
+            fill="rgba(0, 0, 0, 0.2)"
+            x={x}
+            y={0}
+            width={width}
+            height={263}
+        />
+    );
+};
+
+CustomCursor.propTypes = {
+    points: PropTypes.arrayOf(PropTypes.object),
+    width: PropTypes.number,
+    height: PropTypes.number,
+};
+
+
+
 function AverageActivity({ AverageSessionDatas }) {
 
     // Tableau pour convertir les chiffres en jours de la semaine
@@ -27,18 +51,20 @@ function AverageActivity({ AverageSessionDatas }) {
     return (
         <div className="sessions">
             <p className="graphTitle">Durée moyenne des{"\n"} sessions</p>
+            <div className="chartContainer">
             <ResponsiveContainer width={"100%"} height={"100%"}>
                 <LineChart data={AverageSessionDatas}>
                     <XAxis dataKey="day"
                         tickFormatter={(day) => numbersToDays[day]}
                         axisLine={false}
                         tickLine={false}
-                        tick={{ fill: "rgba(255, 255, 255, 1)", dy: -65 }} />
-                    <YAxis yAxisId="left" hide={true} domain={['dataMin - 50', 'dataMax + 10']} />
-                    <Tooltip content={<CustomTooltip />} cursor={false} />
+                        tick={{ fill: "rgba(255, 255, 255, 1)", dy: -5 }} />
+                    <YAxis yAxisId="left" hide={true} domain={['dataMin - 10', 'dataMax + 30']} />
+                    <Tooltip content={<CustomTooltip />} cursor={<CustomCursor />} />
                     <Line yAxisId="left" type="monotone" dataKey="sessionLength" stroke="#FFFFFF" dot={false} />
                 </LineChart>
             </ResponsiveContainer>
+            </div>
         </div>
     );
 }
